27c06de03a9b12f1449f820777959d966686abfc
chentaolu <chentaolu@mail.fcu.edu.tw>
2020-12-15 21:23:01 +0800

    add a client java

--- app/src/main/java/com/example/test/Client.java
+++ app/src/main/java/com/example/test/Client.java
@@ -1,4 +1,47 @@
 package com.example.test;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.*;
+
 public class Client {
+    private String ServerName;
+    private int port;
+    Socket client;
+    String send;
+
+    public Client(String ServerName, int port) {
+        this.ServerName = ServerName;
+        this.port = port;
+        try {
+            client = new Socket(ServerName, port);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            finish();
+        }
+    }
+
+    public void SendMessage(String message) {
+        try {
+            OutputStream outToServer = client.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToServer);
+            out.writeUTF(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String ReadMessage() {
+        try {
+            DataInputStream in = new DataInputStream(this.client.getInputStream());
+            String read = in.readUTF();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
