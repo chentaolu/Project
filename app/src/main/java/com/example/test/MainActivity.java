c284efbfbd7b6aae600d85af56702d8c23b05074
chentaolu <chentaolu@mail.fcu.edu.tw>
2020-12-22 20:38:14 +0800

    can change next page

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,15 +2,27 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Client client;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, MainActivity2.class)));
+
         //this.client = new Client("127.0.0.1", 5678);
+    }
+
+    public Client getClient() {
+        return this.client;
     }
 }
---
+++ app/src/main/java/com/example/test/MainActivity2.java
@@ -0,0 +1,22 @@
+package com.example.test;
+
+import android.os.Bundle;
+//import android.support.wearable.activity.WearableActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private TextView mTextView;
+    private Client client = MainActivity.getClient();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+    }
+}
