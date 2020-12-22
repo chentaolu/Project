b7fdc48b1b6b9a2345d45de5413a463b191701b6
chentaolu <chentaolu@mail.fcu.edu.tw>
2020-12-22 22:12:06 +0800

    add login button and some method page

---
+++ app/src/main/java/com/example/test/searchRecipesByIngredients.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class searchRecipesByIngredients extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_recipes_by_ingredients);
+    }
+}
---
+++ app/src/main/java/com/example/test/searchRecipesByNutrients.java
@@ -0,0 +1,21 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class searchRecipesByNutrients extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_recipes_by_nutrients);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+        // Enables Always-on
+    }
+}
