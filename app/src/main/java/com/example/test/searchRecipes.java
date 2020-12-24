97510093dedf87da46dfae479a007f8fb2c37be6
chentaolu <chentaolu@mail.fcu.edu.tw>
2020-12-25 01:18:04 +0800

    add some button link to page

--- app/src/main/java/com/example/test/searchRecipes.java
+++ app/src/main/java/com/example/test/searchRecipes.java
@@ -2,13 +2,39 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class searchRecipes extends AppCompatActivity {
+public class searchRecipes extends AppCompatActivity implements View.OnClickListener {
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_recipes);
+        ((Button) findViewById(R.id.searchNutrientsButton)).setOnClickListener(this);
+        ((Button) findViewById(R.id.searchVideo)).setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch(v.getId()) {
+            case R.id.searchNutrientsButton:
+                intent = new Intent(searchRecipes.this, searchRecipesByNutrients.class);
+                startActivity(intent);
+                break;
+            case R.id.searchFood:
+                intent = new Intent(searchRecipes.this, searchFoodVideo.class);
+                startActivity(intent);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
+    }
+
 }
