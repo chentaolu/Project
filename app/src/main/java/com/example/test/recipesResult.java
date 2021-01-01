5716fb5903f31a9075f69f76c0a6948f50d6eb6b
GaWei <x981235xyvw@gmail.com>
2021-01-02 00:56:26 +0800

    loginpage

--- app/src/main/java/com/example/test/recipesResult.java
+++ app/src/main/java/com/example/test/recipesResult.java
@@ -2,13 +2,82 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class recipesResult extends AppCompatActivity {
 
+    private LinearLayout mLayout;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipes_result);
+
+        setContentView(R.layout.activity_recipes_result); ///不使用 main.xml 資源
+
+        LinearLayout layout = new LinearLayout(this);
+
+
+        this.addContentView(layout, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT));
+        layout.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout relative = (LinearLayout) findViewById(R.id.imgLayout);
+
+        for(int i = 0; i<5; i++){
+            TextView text = new TextView(this);
+            ImageView img = new ImageView(this);
+            text.setText("123");
+           img.setImageURI("");
+            text.setTextSize(50);
+            relative.addView(text, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+
+
+            EditText txt = new EditText(this);
+            relative.addView(txt, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        }
+
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
