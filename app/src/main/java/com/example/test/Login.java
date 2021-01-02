3ee7cce0081dc80a481954d32acee83c648557d5
GaWei <x981235xyvw@gmail.com>
2021-01-02 20:36:38 +0800

    register page

--- app/src/main/java/com/example/test/Login.java
+++ app/src/main/java/com/example/test/Login.java
@@ -2,13 +2,29 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener{
 
+    private Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ((Button) findViewById(R.id.registerButton)).setOnClickListener(this);
+
+    }
+
+    public void onClick(View v){
+        Intent intent;
+        switch (v.getId()){
+            case R.id.registerButton:
+                intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
